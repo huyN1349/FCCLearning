e0e16eaf78e62f2a26d29cc78fe9091d7ea71cf9
huyN1349 <huy.nguyen1349@gmail.com>
2019-02-12 02:17:02 -0800

    Added JS DOM project for practice

---
+++ DOM/script/index.js
@@ -0,0 +1,3 @@
+document.getElementById("myList").lastElementChild.innerHTML = "Huy";
+document.querySelector("#myList li a").style.color = "red";
+document.querySelector("body button").style.backgroundColor = "yellow";
